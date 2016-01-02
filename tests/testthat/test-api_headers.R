489e4262459e7a183c621ec9188087d3bc7e54d8
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-01 17:41:10 -0800

    added api_headers test

---
+++ tests/testthat/test-api_headers.R
@@ -0,0 +1,37 @@
+context("api_headers")
+
+test_that("api works", {
+  skip_on_cran()
+
+  x <- api('https://api.github.com/') %>%
+    api_headers(`X-FARGO-SEASON` = 3) %>%
+    peep
+
+  y <- api('https://api.github.com/') %>%
+    api_headers(`X-FARGO-SEASON` = three, `Accept Token` = yellow) %>%
+    peep
+
+  yy <- api('https://api.github.com/') %>%
+    api_headers_(`X-FARGO-SEASON` = "three", `Accept Token` = "yellow") %>%
+    peep
+
+  expect_is(x, "http_peep")
+  expect_is(y, "http_peep")
+
+  expect_is(x$url, "url")
+  expect_is(y$url, "url")
+
+  expect_is(x$headers, "list")
+  expect_equal(x$headers$`X-FARGO-SEASON`, "3")
+
+  expect_is(y$headers, "list")
+  expect_named(y$headers, c("X-FARGO-SEASON", "Accept Token"))
+
+  expect_identical(y, yy)
+})
+
+test_that("api fails well", {
+  skip_on_cran()
+
+  expect_error(api_headers(), "argument \".data\" is missing")
+})
