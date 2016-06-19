a8960adef286058fe597dec731e5ecb76c76323a
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-19 10:36:32 -0700

    improvements to paging #2

--- inst/ignore/paging.R
+++ inst/ignore/paging.R
@@ -7,8 +7,28 @@
 #' @param offset Record to start at
 #' @param by Chunk size, if chunking desired. Default:
 #' @family dsl
+#' @section Special Functions:
+#' \itemize{
+#'  \item \code{limit(x)}: \code{x = N}, where x is the name of the API
+#'  variable for determing how many results to return,
+#'  \item \code{limit_max(x)}: xx
+#'  \item \code{offset(x)}: \code{x = N}, where x is the name of the API
+#'  variable for determing what record to start at
+#'  \item \code{page(x)}: what page to return
+#' }
 #' @examples
-#' quer <- api('https://api.github.com/') %>%
+#' url <- 'http://localhost:9200'
+#' quer <-
+#' api(url) %>%
+#'   api_path(shakespeare, `_search`) %>%
+#'   api_paging(limit(size = 10))
+#'
+#' api(url) %>%
+#'   api_path(shakespeare, act, `_search`) %>%
+#'   api_paging(size = 5)
+#'
+#' url <- 'https://api.github.com/'
+#' quer <- api(url) %>%
 #'   api_path(repos, ropensci, rgbif, issues) %>%
 #'   api_query(state = open)
 #'
@@ -28,14 +48,63 @@
 #' # rows & start
 #' # quer %>%
 #' #   api_paging(rows = 10, start = 5)
-api_paging <- function(.data, limit, limit_max, offset = 0, by = NULL) {
+#'
+#' # or could it look like this:
+#' ## YES, this!
+#' api('https://api.github.com/') %>% api_paging(limit(size = 10))
+#'
+#' #### pattern: page/per_page
+#' api('https://api.github.com/') %>%
+#'   api_path(orgs, ropensci, events) %>%
+#'   api_paging(chunk(per_page = 4), page(page = 2)) %>%
+#'   peep
+#'
+#' #### pattern: limit/offset
+#' api('http://api.gbif.org/v1') %>%
+#'   api_path(occurrence, search) %>%
+#'   api_query(scientificName = Accipiter) %>%
+#'   api_paging(limit(limit = 4), offset(offset = 2)) %>%
+#'   peep
+
+api_paging <- function(.data, ..., by = NULL) {
   .data <- as.req(.data)
-  stopifnot(is.numeric(limit), is.numeric(limit_max), is.numeric(offset))
-  by <- get_by(by, limit, limit_max)
-  args <- list(limit = limit, limit_max = limit_max,
-               offset = offset, by = by)
-  modifyList(.data, list(paging = args))
+  # stopifnot(is.numeric(limit), is.numeric(limit_max), is.numeric(offset))
+  # by <- get_by(by, limit, limit_max)
+  # args <- list(limit = limit, size = size, rows = rows, page = page,
+  #              per_page = per_page, limit_max = limit_max,
+  #              start = start, offset = offset, by = by)
+
+  # modifyList(.data, list(paging = list(size = 0, ...)))
+  modifyList(.data, list(paging = list(...)))
 }
+
+limit <- function(...) {
+  lazyeval::all_dots(lazyeval::lazy_dots(...))
+}
+
+offset <- function(...) {
+  lazyeval::all_dots(lazyeval::lazy_dots(...))
+}
+
+page <- function(...) {
+  lazyeval::all_dots(lazyeval::lazy_dots(...))
+}
+
+chunk <- function(...) {
+  lazyeval::all_dots(lazyeval::lazy_dots(...))
+}
+  # vals <- unname(Map(function(x, y) {
+  #   if (nchar(x) == 0) {
+  #     as.character(y$expr)
+  #   }
+  #   else {
+  #     sprintf("%s: %s", x, as.character(y$expr))
+  #   }
+  # }, names(tmp), tmp))
+  # z <- paste0("{", paste0(vals, collapse = ", "), "}")
+  # dots <- comb(tryargs(.data), structure(z, type = "select"))
+  # structure(list(data = getdata(.data), args = dots), class = "jqr")
+#}
 
 get_by <- function(by, limit, limit_max) {
   if (!is.null(by)) {
@@ -49,6 +118,10 @@ get_by <- function(by, limit, limit_max) {
       return(limit)
     }
   }
+}
+
+rename_vars <- function(limit, size, rows) {
+
 }
 
 # ## four headers
